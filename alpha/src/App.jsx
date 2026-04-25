5c90192be0ae34dbd946b98a3a25393c7084285f
SpiceyGabs <sgsquared.2@gmail.com>
2026-04-25 18:21:56 +0200

    FIXED AND SHOWING AND STUFF

--- alpha/src/App.jsx
+++ alpha/src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
+
 import Navbar from './components/Navbar'
-// import Sidebar from './components/Sidebar'
+
 import Home from './pages/Home'
 import MoneySnapshot from './pages/MoneySnapshot'
 import SimLab from './pages/SimLab'
@@ -10,36 +10,21 @@ import StrategyTracks from './pages/StrategyTracks'
 import Learn from './pages/Learn'
 
 function App() {
- 
   return (
-     <BrowserRouter>
-      <div className="app">
-
+    <div className="app">
       <Navbar />
 
-      {/*Then pages */}
-      <main> 
+      <main>
         <Routes>
-       <Route path="/" element={<Home />} />
-            <Route path="/money" element={<MoneySnapshot />} />
-            <Route path="/simlab" element={<SimLab />} />
-            <Route path="/tracks" element={<StrategyTracks />} />
-            <Route path="/learn" element={<Learn />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/money" element={<MoneySnapshot />} />
+          <Route path="/simlab" element={<SimLab />} />
+          <Route path="/tracks" element={<StrategyTracks />} />
+          <Route path="/learn" element={<Learn />} />
         </Routes>
       </main>
-
-      </div>
-        <nav className="navbar">
-          <Link to="/">Home</Link>
-          <Link to="/money">Money Snapshot</Link>
-          <Link to="/simlab">Sim Lab</Link>
-          <Link to="/tracks">Strategy Tracks</Link>
-          <Link to="/learn">Learn</Link>
-        </nav>
-
-    </BrowserRouter>
+    </div>
   )
 }
 
-
-export default App
+export default App;
--- alpha/src/components/Navbar.jsx
+++ alpha/src/components/Navbar.jsx
@@ -1,25 +1,17 @@
- 
-// return
- 
-// (
-    
-// <nav style={{background:'#141414',padding:'12px 24px',display:'flex'}}></nav>
-// )
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 
 function Navbar() {
-    return ( 
-         <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/snapshot">Money Snapshot</NavLink></li>
-          <li><NavLink to="/tracks">Strategy Tracks</NavLink></li>
-          <li><NavLink to="/simulation">Simulation Lab</NavLink></li>
-          <li><NavLink to="/learn">Learn+</NavLink></li>
-        </ul>
-      </nav>
-    )
-};
+  return (
+    <nav className="navbar">
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/money">Money Snapshot</Link></li>
+        <li><Link to="/simlab">Simulation Lab</Link></li>
+        <li><Link to="/tracks">Strategy Tracks</Link></li>
+        <li><Link to="/learn">Learn+</Link></li>
+      </ul>
+    </nav>
+  )
+}
 
-export default Navbar; 
- 
+export default Navbar
